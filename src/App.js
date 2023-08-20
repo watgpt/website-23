853c6ab89ca131793c147e2171d03019db1b7035
Shreyas Peddi <shreyas.peddi@gmail.com>
2023-08-20 13:41:58 -0400

    fixed footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AppNavbar from './components/AppNavbar';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap'; // Import Container component
 
 function App() {
   return (
-    <div className="mx-5 my-3">
-    <AppNavbar/>
-    <div>
+    <div className="mx-5 my-3 d-flex flex-column" style={{ minHeight: '98vh' }}>
+      <AppNavbar />
+      <Container className="flex-grow-1"> {/* Use flex-grow-1 to fill remaining space */}
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-    </div>
-    <Footer />
+      </Container>
+      <Footer className="footer" />
     </div>
   );
 }
