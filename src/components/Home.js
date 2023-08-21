256cf3e6b73ffdb664898ecb3cdf29141d681a34
Shreyas Peddi <shreyas.peddi@gmail.com>
2023-08-21 18:38:40 -0400

    updated description

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ function Home(){
             <Stack gap={3} className="landing-stack">
                 <Image src={require(`../assets/watgpt-logo.png`)} width={200} alt="logo"/>
                 <p className='tagline'>Creating GPT Powered Solutions For Every Student</p>
-                <p className="col-md-6">We develop GPT trained software and chatbots that are practical to use for University of Waterloo students. We are a student design team at the University of Waterloo and a member of the Sedra Student Design Centre. </p>
+                <p className="col-md-6">We develop GPT trained software and chatbots that are practical to use for University of Waterloo students. We are a student design team at the University of Waterloo and a member of the Sedra Student Design Centre. Our potential upcoming projects include working on Career Hub Chatbot and tools within WaterlooWorks. We actively collaborate with startups to build strong partnerships and create meaningful impact. We aim to provide opportunities to learn and build practical expertise by creating projects for students, by students. </p>
                 <Button className="py-2" variant="outline-light" onClick={handleJoinClick}>join us</Button>
             </Stack>
     );
